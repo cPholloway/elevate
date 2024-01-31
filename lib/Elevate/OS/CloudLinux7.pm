c987e42d0a685cab8057b3045850c67ee99a94d4
Travis Holloway <t.holloway@cpanel.net>
2024-01-31 09:52:05 -0600

    Add initial support for Elevating from CL7 to CL8
    Case RE-18: Add initial support for Elevating from CL7 to CL8
    
    * Add Elevate::OS library to handle the heavy lifting regarding OS
    related inquiries
    * Do not block if the OS to elevate is CloudLinux 7
    * Teach the '--upgrade-to' option abobut CloudLinux 7 and additional OS
    based parsing for it
    
    Changelog: Add initial support for Elevating from CL7 to CL8

---
+++ lib/Elevate/OS/CloudLinux7.pm
@@ -0,0 +1,25 @@
+package Elevate::OS::CloudLinux7;
+
+=encoding utf-8
+
+=head1 NAME
+
+Elevate::OS::CloudLinux7 - CloudLinux7 custom values
+
+=cut
+
+use cPstrict;
+
+use Log::Log4perl qw(:easy);
+
+use parent 'Elevate::OS::Base';
+
+sub _build_get_default_upgrade_to ($self) {
+    return 'cloudlinux';
+}
+
+sub _build_name ($self) {
+    return 'CloudLinux7';
+}
+
+1;
